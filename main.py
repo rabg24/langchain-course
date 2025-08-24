3a2491076e9a4ac7bdf4009675d2f15b904651da
Bobby Gupta <guptabobby1819@gmail.com>
2025-08-24 22:48:12 +0100

    environment setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+# import os
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+#   print(os.environ.get("OPENAI_API_KEY", "Environment variable not set."))
+
+
+if __name__ == "__main__":
+    main()
